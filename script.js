923f91b4f8ff2ba41906032fb2a898fc1d3681b9
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-28 16:30:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function volume_sphere() {
-    //Write your code here
-  
+function volume_sphere(){
+     let temp = document.getElementById('radius').value
+     if(parseInt(temp) < 0 || typeof temp === "string"){
+		 temp = NaN
+	 }
+	else{temp = (4/3) * Math.PI * Math.pow(parseInt(temp), 3)}  
+     document.getElementById('volume').value = temp
+    
+   
+	
+
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
