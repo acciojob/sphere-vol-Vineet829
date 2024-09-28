1aa2484e43cfaf620e92e280e363b227cf05d34f
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-28 16:38:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function volume_sphere(){
-     let temp = document.getElementById('radius').value
-     if(parseInt(temp) < 0 || typeof temp === "string"){
-		 temp = NaN
-	 }
-	else{temp = (4/3) * Math.PI * Math.pow(parseInt(temp), 3)}  
-     document.getElementById('volume').value = temp
-    
-   
-	
+function volume_sphere(event) {
+    event.preventDefault();
+    let temp = document.getElementById('radius').value;
+    temp = parseFloat(temp);
+    if (temp < 0 || isNaN(temp)) {
+        temp = NaN;
+    } else {
+        temp = (4 / 3) * Math.PI * Math.pow(temp, 3);
+    }
+    document.getElementById('volume').value = temp;
+}
 
-} 
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+    document.getElementById('MyForm').onsubmit = volume_sphere;
+};
